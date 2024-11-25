79108ad96e04f755dcaa01641899e0c89d6e0dab
Ubuntu <ubuntu@ip-172-31-24-46.ec2.internal>
2024-11-25 22:05:47 +0000

    Updated orchestration scripts to use IMDSv2

--- orchestration/load-fpga-image.sh
+++ orchestration/load-fpga-image.sh
@@ -2,8 +2,16 @@
 
 echo "Starting FPGA load image..."
 
-instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-instancetype=$(curl http://169.254.169.254/latest/meta-data/instance-type)
+# updated to use IMDSv2
+TOKEN3=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+        && curl -H "X-aws-ec2-metadata-token: $TOKEN3" http://169.254.169.254/latest/meta-data/
+
+instanceid=$(curl -H "X-aws-ec2-metadata-token: $TOKEN3" http://169.254.169.254/latest/meta-data/instance-id)
+instancetype=$(curl -H "X-aws-ec2-metadata-token: $TOKEN3" http://169.254.169.254/latest/meta-data/instance-type)
+region=$(curl -H "X-aws-ec2-metadata-token: $TOKEN3" http://169.254.169.254/latest/meta-data/placement/region)
+
+#instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+#instancetype=$(curl http://169.254.169.254/latest/meta-data/instance-type)
 
 if [ "$#" -ne 1 ]
 then
@@ -18,9 +26,9 @@ echo "AGFI id : $1"
 sudo fpga-load-local-image -S 0 -I $1 -H
 
 duration=$SECONDS
-echo "$duration seconds elapsed."
+echo "$duration seconds elapsed to load image."
 
-aws cloudwatch put-metric-data --namespace "FPGA metrics" --metric-name fpgaLoadImageTime --value $duration --unit Seconds --dimensions InstanceID=$instanceid,InstanceType=$instancetype
+aws cloudwatch put-metric-data --namespace "FPGA metrics" --metric-name --region $region fpgaLoadImageTime --value $duration --unit Seconds --dimensions InstanceID=$instanceid,InstanceType=$instancetype
 
 
 
